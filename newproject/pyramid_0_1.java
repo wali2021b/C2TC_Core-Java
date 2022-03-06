8bd4158d019d490c194d3462020db24cab72d8a3
Sayyed Waliullah <100829970+wali2021b@users.noreply.github.com>
2022-03-06 19:48:20 +0530

    external commit againnnnnn..

---
+++ newproject/pyramid_0_1.java
@@ -0,0 +1,24 @@
+package jjjjj0001;
+
+public class pyramid_0_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int k;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				k=i+j;
+				if(k%2==1)
+					System.out.print(" "+0);
+				else
+					System.out.print(" "+1);
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
